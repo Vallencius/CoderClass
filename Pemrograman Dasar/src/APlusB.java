226199a584277b676f1f3ae1731f02e93d44d8c8
Vallencius <vallencius1901@gmail.com>
2022-08-21 15:33:17 +0700

    Pemrograman Dasar

---
+++ Pemrograman Dasar/src/APlusB.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class APlusB {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long a = scan.nextLong();
+        long b = scan.nextLong();
+
+        scan.close();
+
+        System.out.println(a+b);
+    }
+}
---
+++ Pemrograman Dasar/src/Berhitung.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Berhitung {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        char op = scan.next();
+        int b = scan.nextInt()
+    }
+}

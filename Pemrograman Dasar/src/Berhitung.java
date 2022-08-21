38e77a9c0937d1652f8d6331063210b33e37e81f
Vallencius <vallencius1901@gmail.com>
2022-08-21 16:06:34 +0700

    Berhitung

--- Pemrograman Dasar/src/Berhitung.java
+++ Pemrograman Dasar/src/Berhitung.java
@@ -4,7 +4,25 @@ public class Berhitung {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // char op = scan.next();
+        char op = scan.next().charAt(0);
         int b = scan.nextInt();
+
+        switch(op){
+            case('+'):
+                System.out.println(a+b);
+                break;
+            case('-'):
+                System.out.println(a-b);
+                break;
+            case('/'):
+                System.out.println(a/b);
+                break;
+            case('*'):
+                System.out.println(a*b);
+                break;
+            case('%'):
+                System.out.println(a%b);
+                break;
+        }
     }
 }

2f4b0598c1d384ce16ff15ac9aeddbfe5b416a2e
antoniuskevin15 <antoniuskevin15@gmail.com>
2022-08-21 15:51:05 +0700

    1c

--- Pemrograman Dasar/src/Berhitung.java
+++ Pemrograman Dasar/src/Berhitung.java
@@ -4,7 +4,7 @@ public class Berhitung {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        char op = scan.next();
+        // char op = scan.next();
         int b = scan.nextInt();
     }
 }

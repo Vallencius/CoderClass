7e202ae664ccc8c7238f3095ae4793e88b19b100
atras30 <atrasshalhan@gmail.com>
2022-08-21 17:06:07 +0700

    array - jungkir balik done

---
+++ Pemrograman Dasar/src/JungkirBalik.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class JungkirBalik {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int[] array = new int[n];
+
+    int arrayIndex = 0;
+    for(int i = 0; i < n; i++) {
+      array[arrayIndex++] = scanner.nextInt();
+    }
+
+    for(int i = n-1; i >= 0; i--) {
+      System.out.println(array[i]);
+    }
+  }
+}
